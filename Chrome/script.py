336f228820bd5c7d70ee6200aa88ea84e807731d
aliakbar09a <aliakbar09a@gmail.com>
2018-10-03 09:30:56 +0530

    final commit before commenting

--- Chrome/script.py
+++ Chrome/script.py
@@ -4,14 +4,34 @@ import cv2
 import time
 import random
 
-mess_count = 0
+total_mess_count = 0
 send_count = 0
-no_of_pages = 12
+old_count = 0
+no_of_pages = 5
 
-string = 'Thank you for connecting, Glad to connect, Congratulations for all that you do and especially for just who you are. My best wishes to you for your journeys into the future. Please stay in touch; taking unexpected folks in the road can create spectacular destinations.Best Regards, Ali'
+message = 'Thank you for connecting, Glad to connect, Congratulations for all that you do and especially for just who you are. My best wishes to you for your journeys into the future. Please stay in touch; taking unexpected folks in the road can create spectacular destinations.'
+message_end = 'Best Regards, Ali.'
 
 
-def clickAllCloseButton(safety):
+def isNew():
+    # message window parameters
+    window_tuple = (725, 500, 300, 150)
+    # getting a screenshot of the chat window
+    window_image = pg.screenshot(region=window_tuple)
+    # converting to array
+    window_array = np.array(window_image.getdata(), dtype='uint8').reshape(window_image.size[0],
+                                                                           window_image.size[1], 3)
+    # converting color to grayscale
+    gray = cv2.cvtColor(window_array, cv2.COLOR_BGR2GRAY)
+    gray = gray / 255
+    # checking if the window is full white
+    if(gray.sum() == 45000.0):
+        return True
+    else:
+        return False
+
+
+def clickAllCloseButton():
     black = 0
     white = 0
     while(True):
@@ -24,8 +44,9 @@ def clickAllCloseButton(safety):
         pg.moveTo(x, y, 0.25)
         pg.click()
         white = white + 1
+        time.sleep(0.1)
     while(True):
-        blackCloseBlank = pg.locateOnScreen('blackCloseBlank.png')
+        blackCloseBlank = pg.locateOnScreen('blackClose.png')
         if blackCloseBlank == None:
             break
         x, y, width, height = blackCloseBlank
@@ -34,6 +55,7 @@ def clickAllCloseButton(safety):
         pg.moveTo(x, y, 0.25)
         pg.click()
         black = black + 1
+        time.sleep(0.1)
     while(True):
         blackCloseShort = pg.locateOnScreen('blackCloseShort.png')
         if blackCloseShort == None:
@@ -44,6 +66,7 @@ def clickAllCloseButton(safety):
         pg.moveTo(x, y, 0.25)
         pg.click()
         black = black + 1
+        time.sleep(0.1)
     while(True):
         blackCloseLong = pg.locateOnScreen('blackCloseLong.png')
         if blackCloseLong == None:
@@ -54,6 +77,7 @@ def clickAllCloseButton(safety):
         pg.moveTo(x, y, 0.25)
         pg.click()
         black = black + 1
+        time.sleep(0.1)
     # print the no. of black close and white close encountered
     print("black ", black, " white ", white)
 
@@ -84,33 +108,37 @@ def clickSend():
     return
 
 
+def writeMessage():
+    # either write by copy pasting
+    pg.hotkey('ctrl', 'v')
+    # or write by typing
+    #pg.typewrite(message, interval=0.01)
+    # pg.press('enter')
+    #pg.typewrite(message_end, interval=0.01)
+
+
 pg.hotkey('altleft', '\t')  # to get the browser on the screen
 time.sleep(1)
 for i in range(no_of_pages):
+    clickAllCloseButton()
     list_of_message_button = pg.locateAllOnScreen('message.png')
-    clickAllCloseButton(False)
     for button in list_of_message_button:
         left, top, width, height = button
         xbutton, ybutton = left + int(width/2), top + int(height/2)
         pg.moveTo(xbutton, ybutton, 0.5)
         pg.click()
-        mess_count = mess_count + 1
-        time.sleep(3)
-        # message window parameters
-        window_tuple = (725, 500, 300, 150)
-        window_image = pg.screenshot(region=window_tuple)
-        window_array = np.array(window_image.getdata(), dtype='uint8').reshape(window_image.size[0],
-                                                                               window_image.size[1], 3)
-        gray = cv2.cvtColor(window_array, cv2.COLOR_BGR2GRAY)
-        gray = gray / 255
-        if(gray.sum() == 45000.0):
-            # pg.typewrite(string, interval=0.01)
-            pg.hotkey('ctrl', 'v')
+        total_mess_count = total_mess_count + 1
+        time.sleep(2)
+        found = isNew()
+        if(found):
+            writeMessage()
             clickSend()
             send_count = send_count + 1
-        clickAllCloseButton(True)
-    time.sleep(0.5)
+        if(not found):
+            old_count = old_count + 1
+        clickAllCloseButton()
     pg.press('pagedown')
-    time.sleep(3)
-print(mess_count, "mess_count")
-print(send_count, "send_count")
+    time.sleep(1.5)
+    print(total_mess_count, "total message button clicked")
+    print(old_count, "old connection found")
+    print(send_count, "send button clicked")
